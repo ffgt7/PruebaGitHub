fc517fb8952a43fb788035af7b34d156dd2a3742
ffgt7 <unah20121015078@gmail.com>
2017-06-27 19:29:58 -0600

    Creacion del Fragment

---
+++ app/src/main/java/com/example/f_t/pruebagithub/PruebaFragmento.java
@@ -0,0 +1,29 @@
+package com.example.f_t.pruebagithub;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PruebaFragmento extends Fragment {
+
+
+    public PruebaFragmento() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_prueba_fragmento, container, false);
+    }
+
+}
